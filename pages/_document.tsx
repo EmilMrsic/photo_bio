5e4969dc989c2c0c13ae59215daaaa1b4b4d72ac
EmilMrsic <130184003+EmilMrsic@users.noreply.github.com>
2025-07-22 09:38:03 -0400

    Improve login page and setup env

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,1 +1,15 @@
-<link rel='stylesheet' href='https://rsms.me/inter/inter.css' />
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en" className="bg-white dark:bg-gray-900">
+      <Head>
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+      </Head>
+      <body className="min-h-screen font-sans antialiased">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
